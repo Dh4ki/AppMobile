cbf3001b42fcaa2a8cdd42dba38ae90b4f0833be
Dh4ki <edu_gv_14@hotmail.com>
2023-07-17 03:52:31 -0500

    CREACION DE PROYECTO
    Se crea el proyecto y se da un primer avance en el diseño de la pantalla
    principal.

---
+++ app/src/main/java/com/eduardo/appmobile/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.eduardo.appmobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.eduardo.appmobile.ui.theme.AppMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppMobileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginContent(){
+    Box(modifier = Modifier.fillMaxSize()){
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.logo_golosinas),
+            contentDescription = "Imagen de fondo",
+            contentScale = ContentScale.Crop,
+            colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply {
+                setToScale( redScale = 0.5f, greenScale = 0.5f, blueScale = 0.5f, alphaScale = 0.85f)
+            })
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 70.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .height(170.dp)
+                    .width(170.dp),
+                painter = painterResource(id = R.drawable.golosinas),
+                contentDescription = "Logo"
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 7.dp),
+                text = "Bienvenidos a                                      EL MUNDO AZUCARADO",
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                fontSize = 22.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    AppMobileTheme {
+        Greeting("Android")
+    }
+}
